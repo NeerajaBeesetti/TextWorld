466db640a40308067ed1c5a1091741af1a62c4c5
10kookies <neerajabeesetti10@gmail.com>
2019-04-13 19:55:42 -0700

    deleted a comment

--- src/Popstar.java
+++ src/Popstar.java
@@ -6,21 +6,21 @@ public class Popstar extends Creature {
     }
 
     public void move() {
-        //TODO: fix popstar!
         if (currentRoom.containsNeighbor(playerRoom.getName())) {
             moveToRoom(playerRoom);
-            System.out.println(getName() + " moved to " + playerRoom.getName());
+            System.out.println(getName() + " is in your room");
         } else if (!(currentRoom.containsNeighbor(playerRoom.getName()))) {
             for (Level.Room temp : currentRoom.getNeighborList()) {
                 if (temp.containsNeighbor(playerRoom.getName())) {
                     moveToRoom(temp);
                     System.out.println(getName() + " moved to " + temp.getName());
+                    break;
                 }
             }
         } else {
             Level.Room newRoom = currentRoom.getRandomNeighbor();
             moveToRoom(newRoom);
-            System.out.println(getName() + " moved to " + newRoom.getName());
+            System.out.println(getName() + " moved randomly to " + newRoom.getName());
         }
     }
 }
